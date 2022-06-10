e61ebd484e5caed92bf870ac83b342e50cbbcda6
unknown <rafael.mendes112@hotmail.com>
2022-06-09 21:42:16 -0300

    Finalizando o Alura-Midi

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    if (elemento !== null && elemento.localName === 'audio') {
+            elemento.play();
+        } else {
+            console.log('Elemento não encontrado ou seletor');
+        }
+    }   
+
+
 
 
 const listaDeTeclas =  document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
+
+// para
+for (let contador =0; contador < listaDeTeclas.length;contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        // console.log(evento);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
 
